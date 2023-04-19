5fada378c76c41cd02c9bad3dd8c1799f82e84bc
Ulf Gebhardt <ulf.gebhardt@webcraft-media.de>
2023-04-19 13:13:50 +0200

    branding

---
+++ branding/constants/metadata.js
@@ -0,0 +1,9 @@
+// this file is duplicated in `backend/src/config/metadata.js` and `webapp/constants/metadata.js` and replaced on rebranding
+export default {
+  APPLICATION_NAME: 'yunite.me',
+  APPLICATION_SHORT_NAME: 'yunite.me',
+  APPLICATION_DESCRIPTION: 'yunite.me – Alternatives Netzwerk für eine freie und bunte Zukunft',
+  COOKIE_NAME: 'yunite-me-token',
+  ORGANIZATION_NAME: 'yunite – Verein für Vernetzung und Kooperation',
+  ORGANIZATION_JURISDICTION: 'Switzerland',
+}
